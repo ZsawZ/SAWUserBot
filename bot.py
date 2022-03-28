79c7273043a496733dd47962f8c9878ac8ed852a
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-03-28 20:02:51 +0300

    update bot.py

--- bot.py
+++ bot.py
@@ -1217,10 +1217,7 @@ async def mute_hammer(client: Client, message: Message):
 unmute_permissions = ChatPermissions(
     can_send_messages=True,
     can_send_media_messages=True,
-    can_send_stickers=True,
     can_send_animations=True,
-    can_send_games=True,
-    can_use_inline_bots=True,
     can_add_web_page_previews=True,
     can_send_polls=True,
     can_change_info=False,
