0c4809a7df989ddde0b1dfeb79056d5177b063a3
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-01 20:26:39 +0300

    update bot.py

--- bot.py
+++ bot.py
@@ -1725,7 +1725,7 @@ async def demotivator(client, message):
  
     if message.reply_to_message.photo:
         await client.unblock_user("memegeneration_bot")
-        capt = "1. " + message.text.split(prefix + "dem ", maxsplit=1)[1]
+        capt = "1. " + message.text.split(. + "dem ", maxsplit=1)[1]
         await client.send_photo(
             chat_id="memegeneration_bot",
             photo=message.reply_to_message.photo.file_id,
