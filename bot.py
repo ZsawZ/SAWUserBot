5cf6bacc83785ab2a40f6a01a7bb7565dbf69685
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-01 20:01:38 +0300

    update bot.py

--- bot.py
+++ bot.py
@@ -1717,6 +1717,33 @@ async def shell(_, message: Message):
         text += f"<b>Completed in {round(stop_time - start_time, 5)} seconds with code {cmd_obj.returncode}</b>"
     await message.edit(text)
     cmd_obj.kill()
+    
+#demotivator
+@app.on_message(filters.command("dem", prefixes=".") & filters.me)
+async def demotivator(client, message):
+    await message.edit("Создание демотиватора..")
+ 
+    if message.reply_to_message.photo:
+        await client.unblock_user("memegeneration_bot")
+        capt = "1. " + message.text.split(prefix + "dem ", maxsplit=1)[1]
+        await client.send_photo(
+            chat_id="memegeneration_bot",
+            photo=message.reply_to_message.photo.file_id,
+            caption=capt
+        )
+        photo = False
+ 
+        while not photo:
+            try:
+                await asyncio.sleep(2)
+                iii = await client.get_history("memegeneration_bot")
+                await client.send_photo(chat_id=message.chat.id, photo=iii[0].photo.file_id)
+                photo = True
+                await message.delete()
+            except:
+                await asyncio.sleep(2)
+    else:
+        await message.edit("Сделайте реплай на фото")
 
 
 app.run()
