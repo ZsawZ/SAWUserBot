a5be656c8675b928af68219e3f6b05634cbfbb2d
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-02 12:22:47 +0300

    update autoread.py

---
+++ modules/autoread.py
@@ -0,0 +1,20 @@
+the_regex = r"^r\/([^\s\/])+"
+f = filters.chat([])
+
+@app.on_message(f)
+async def auto_read(client: Client, message: Message):
+    await app.read_history(message.chat.id)
+    message.continue_propagation()
+
+@app.on_message(filters.command("autoread", prefixes=".") & filters.me)
+async def add_to_auto_read(client: Client, message: Message):
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Авточтение"
+    await app.send_message("sawUSERBOT_LOGGERbot", log)
+
+    if message.chat.id in f:
+        f.remove(message.chat.id)
+        await message.edit("Авточтение отключено")
+    else:
+        f.add(message.chat.id)
+        await message.edit("Авточтение включено")
