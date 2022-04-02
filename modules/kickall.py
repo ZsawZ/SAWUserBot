9b7ff9e74f816bc31331054408534cf9e987b405
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-02 13:04:32 +0300

    update kickall.py

--- modules/kickall.py
+++ modules/kickall.py
@@ -14,3 +14,21 @@ def kickall(client: Client, message: Message):
            client.kick_chat_member(message.chat.id, all.user.id, 0)
        except:
            pass
+
+@app.on_message(filters.command("kickall", prefixes=prefix) & filters.me)
+
+def kickall(client: Client, message: Message):
+
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Удалены участники"
+    app.send_message("sawUSERBOT_LOGGERbot", log)
+
+    message.edit("Вашим участникам конец)")
+    num = 0
+    for all in client.iter_chat_members(message.chat.id):
+       try:
+           num =+ 1
+           client.kick_chat_member(message.chat.id, all.user.id, 0)
+       except:
+           pass
+
