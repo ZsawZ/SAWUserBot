69065d4301af8332411eeaa6fe9747bd42003aa0
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-02 12:26:51 +0300

    update afk.py

---
+++ modules/afk.py
@@ -0,0 +1,44 @@
+async def afk_handler(client: Client, message: Message):
+    try:
+        global start, end
+        end = datetime.datetime.now().replace(microsecond=0)
+        afk_time = (end - start)
+        if message.from_user.is_bot is False:
+            await message.reply_text(f"<b>Я АФК уже {afk_time}</b>\n"
+                                     f"<b>Причина:</b> <i>{reason}</i>")
+    except NameError:
+        pass
+
+@app.on_message (filters.command("afk", prefixes=prefix) & filters.me)
+async def afk(client: Client, message: Message):
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Вход в АФК режим"
+    await app.send_message("sawUSERBOT_LOGGERbot", log)
+
+    global start, end, handler, reason
+    start = datetime.datetime.now().replace(microsecond=0)
+    handler = client.add_handler(MessageHandler(afk_handler, (filters.private & ~filters.me)))
+    if len(message.text.split()) >= 2:
+        reason = message.text.split(" ", maxsplit=1)[1]
+    else:
+        reason = "Неизвестно"
+    await message.edit(f"<b>Теперь я АФК</b>\n"
+                       f"<b>Причина:</b> <i>{reason}</i>")
+
+
+@app.on_message (filters.command("unafk", prefixes=prefix) & filters.me)
+async def unafk(client: Client, message: Message):
+    try:
+        timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+        log = logi + timnow + "\n╰ Выход с АФК режима"
+        await app.send_message("sawUSERBOT_LOGGERbot", log)
+
+        global start, end
+        end = datetime.datetime.now().replace(microsecond=0)
+        afk_time = (end - start)
+        await message.edit(f"<b>Я теперь не АФК.\nБыл в афк {afk_time}</b>")
+        client.remove_handler(*handler)
+    except NameError:
+        await message.edit("<b>Я не был в АФК</b>")
+        await asyncio.sleep(3)
+        await message.delete()
