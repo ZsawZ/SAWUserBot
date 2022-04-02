981c8b67c50d5b5d577db6e67cbf4c09970a1b55
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-02 12:08:56 +0300

    update drugs.py

---
+++ modules/drugs.py
@@ -0,0 +1,38 @@
+@app.on_message(filters.command("drugs", prefixes=prefix) & filters.me)
+
+async def drugs(client: Client, message: Message):
+
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Комманда drugs"
+    await app.send_message("sawUSERBOT_LOGGERbot", log)
+
+    perc = 0
+    result = 0
+    while(perc < 100):
+        try:
+            text = "🍁Поиск запрещённых препаратов " + str(perc) + "%"
+            await message.edit(str(text))
+            perc += random.randint(1, 3)
+            await asyncio.sleep(0.1)
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+    text = "Найдено 3 кг шпекса🍪💨"
+    await message.edit(str(text))
+    await asyncio.sleep(3)
+    text = "Оформляем вкид 🌿⚗️"
+    await message.edit(str(text))
+    await asyncio.sleep(5)
+    result += random.randint(1, 4)
+
+    if result == 1:
+        text = "🔥😳 Вас успешно откачали, пожалуйста, больше не принимайте запрещённые препараты 😳🔥"
+        await message.edit(str(text))
+    if result == 2:
+        text = "🥴Вы пожилой наркоман, вас не берёт одна доза, вам необходимо больше, попробуйте  ещё раз оформить вкид🥴"
+        await message.edit(str(text))
+    if result == 3:
+        text = "😖Сегодня не ваш день, вы хоть и пожилой, но приняли слишком много. Окончательная причина смерти - передоз😖"
+        await message.edit(str(text))
+    if result == 4:
+        text = "😌Вы оформили вкид, Вам понравилось)😌"
+        await message.edit(str(text))
