9ae9f1eef1db15cb376a72f86c15f0f8bd4e6a9e
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-02 12:47:41 +0300

    update weather.py

---
+++ modules/weather.py
@@ -0,0 +1,29 @@
+def get_pic(city):
+
+    file_name = f"{city}.png"
+
+    with open(file_name, "wb") as pic:
+        response = requests.get("http://wttr.in/{citys}_2&lang=ru.png", stream=True)
+
+        if not response.ok:
+            print(response)
+
+        for block in response.iter_content(1024):
+            if not block:
+                break
+
+            pic.write(block)
+        return file_name
+
+@app.on_message(filters.command("weather", prefixes=".") & filters.me)
+async def weather(client: Client, message: Message):
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Погода"
+    await app.send_message("sawUSERBOT_LOGGERbot", log)
+
+    city = message.command[1]
+    await message.edit("```Загрузка...```")
+    r = requests.get(f"https://wttr.in/{city}?m?M?0?q?T&lang=ru")
+    await message.edit(f"```City: {r.text}```")
+    await client.send_photo(chat_id=message.chat.id, photo=get_pic(city), reply_to_message_id=message.message_id)
+    os.remove(f"{city}.png")
