120c4d07815fd6952cfd8cdabf3c4dbcd2bc0f38
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-02 13:44:16 +0300

    update id.py

---
+++ modules/id.py
@@ -0,0 +1,14 @@
+@app.on_message(filters.command("id", prefixes=prefix) & filters.me)
+
+async def id(client: Client, message: Message):
+
+    now = datetime.datetime.now()
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Комманда id"
+    await app.send_message("sawUSERBOT_LOGGERbot", log)
+
+    if message.reply_to_message is None:
+        await message.edit(f"Айди: {message.chat.id}")
+    else:
+        id = f"Айди: {message.reply_to_message.from_user.id}\nАйди чата: {message.chat.id}"
+        await message.edit(id)
