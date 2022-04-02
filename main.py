17c7d68648604c4a31697dbf2bc54773aa21125c
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-02 18:35:02 +0300

    update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     api_id, api_hash, device_mod = my_api()
     prestart()
     plugins = dict(root="modules")
-    Client = Client("my_account", api_id=api_id, api_hash=api_hash, device_model=device_mod, plugins=plugins).run()
     app = Client("my_account", api_id=api_id, api_hash=api_hash, device_model=device_mod, plugins=plugins).run()
     with app:
          app.join_chat("SAWuser_bot") 
