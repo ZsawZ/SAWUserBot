e46926ec00341751293d47bc887e8f821f0e49e7
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-03 14:32:01 +0300

    update ladder.py

--- modules/ladder.py
+++ modules/ladder.py
@@ -1,16 +1,19 @@
-@app.on_message(filters.command("ladder", prefixes=prefix) & filters.me)
+@Client.on_message(filters.command("ladder", prefixes=prefix) & filters.me)
 
 async def ladder(client: Client, message: Message):
 
 
     timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
     log = logi + timnow + "\n╰ Команда ladder"
-    await app.send_message("sawUSERBOT_LOGGERbot", log)
+    await Client.send_message("sawUSERBOT_LOGGERbot", log)
 
     orig_text = message.text.split("." + "ladder ", maxsplit=1)[1]
     text = orig_text
     output = []
     for i in range(len(text) + 1):
-     output.append(text[:i])
+     output.Clientend(text[:i])
     ot = "\n".join(output)
     await message.edit(ot)
+
+module_list['Ladder'] = f'{prefix}ladder | {'
+file_list['Bomber'] = 'bomber.py'
