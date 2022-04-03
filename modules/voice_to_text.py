a2a9ad4b39deaa3cb4a31ae5c257103cf1bd3ffb
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-03 15:03:08 +0300

    update voice_to_text.py

--- modules/voice_to_text.py
+++ modules/voice_to_text.py
@@ -1,4 +1,4 @@
-@app.on_message(filters.command("text", prefixes=prefix) & filters.me)
+@Client.on_message(filters.command("text", prefixes=prefix) & filters.me)
 
 async def gstotext(client: Client, message: Message):
 
@@ -8,11 +8,14 @@ async def gstotext(client: Client, message: Message):
 
     timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
     log = logi + timnow + "\n╰ Переведено голосовое в текст"
-    await app.send_message("sawUSERBOT_LOGGERbot", log)
+    await Client.send_message("sawUSERBOT_LOGGERbot", log)
 
     await message.edit("Пишу текстом...")
     await message.reply_to_message.forward("VoiceMsgBot")
     await asyncio.sleep(5)
-    iii = await app.get_history("VoiceMsgBot")
+    iii = await Client.get_history("VoiceMsgBot")
     await message.edit("Отправка текста...")
-    await app.forward_messages(message.chat.id, "VoiceMsgBot", iii[0].message_id)
+    await Client.forward_messages(message.chat.id, "VoiceMsgBot", iii[0].message_id)
+
+module_list['Voice to text'] = f'{prefix}text'
+file_list['Vo'] = 'bomber.py'
