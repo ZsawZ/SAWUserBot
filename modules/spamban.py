2b56c1d43ec48c827b2fb01066b85c86486ffbcb
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-03 14:55:31 +0300

    update spamban.py

--- modules/spamban.py
+++ modules/spamban.py
@@ -1,15 +1,18 @@
-@app.on_message(filters.command("spamban", prefixes=prefix) & filters.me)
+@Client.on_message(filters.command("spamban", prefixes=prefix) & filters.me)
 
 async def spamban(client: Client, message: Message):
 
 
     timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
     log = logi + timnow + "\n╰ Проверка нарушений"
-    await app.send_message("sawUSERBOT_LOGGERbot", log)
+    await Client.send_message("sawUSERBOT_LOGGERbot", log)
 
     await message.edit("Чекаю твой акк на наличие спамбана")
-    await app.send_message("spambot", "/start")
+    await Client.send_message("spambot", "/start")
     await asyncio.sleep(1)
-    iii = await app.get_history("spambot")
+    iii = await Client.get_history("spambot")
     await message.delete()
-    await app.forward_messages(message.chat.id, "spamBot", iii[0].message_id)
+    await Client.forward_messages(message.chat.id, "spamBot", iii[0].message_id)
+
+module_list['Spamban'] = f'{prefix}spamban'
+file_list['Bomber'] = 'bomber.py'
