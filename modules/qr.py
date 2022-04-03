e8c1b111df18051cbaba64920f1c3301100d0a71
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-03 14:40:51 +0300

    update qr.py

--- modules/qr.py
+++ modules/qr.py
@@ -11,19 +11,22 @@ def get_cmd_content(message: Message):
         content = ""
     return content
 
-@app.on_message(filters.command("qr", prefixes=prefix) & filters.me & content_filter)
+@Client.on_message(filters.command("qr", prefixes=prefix) & filters.me & content_filter)
 async def qr_cmd(client: Client, message: Message):
     timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
     log = logi + timnow + "\n╰ Создан qr-code"
-    await app.send_message("sawUSERBOT_LOGGERbot", log)
+    await Client.send_message("sawUSERBOT_LOGGERbot", log)
 
     text = get_cmd_content(message)
     await message.delete()
     async with ClientSession() as session:
         async with session.head("https://api.qrserver.com/v1/create-qr-code/", params={"data": text}) as resp:
-            await app.send_photo(
+            await Client.send_photo(
                 chat_id=message.chat.id,
                 photo=str(resp.url),
                 caption=text,
                 parse_mode=None,
             )
+
+module_list['QRcode'] = f'{prefix}qr'
+file_list['QR'] = 'bomber.py'
