4300d175341ff0a15752665578fd0a17d8526bc5
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-04 20:33:00 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cat > .env << EOL
-API_ID=${2860432}
-API_HASH=${2fde6ca0f8ae7bb58844457a239c7214
+API_ID=$2860432
+API_HASH=$2fde6ca0f8ae7bb58844457a239c7214
 
 # sqlite/sqlite3
 DATABASE_TYPE=$db_type
