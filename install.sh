890bef225f56d0f405cb29b5df363279d44e9a61
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-04 20:33:46 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ API_ID=$2860432
 API_HASH=$2fde6ca0f8ae7bb58844457a239c7214
 
 # sqlite/sqlite3
-DATABASE_TYPE=$db_type
+DATABASE_TYPE=$sqlite3
 # file name for sqlite3
 DATABASE_NAME=$db.sqlite3
 EOL
