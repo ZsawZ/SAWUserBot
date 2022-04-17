924ec4b9154630bdcb973e266d563382467dafa3
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-17 11:20:20 +0300

    update bot.py

--- bot.py
+++ bot.py
@@ -37,12 +37,10 @@ from pyrogram.utils import (
 )
 import base64
 from io import BytesIO
- 
 import requests
 from pyrogram import Client, filters, errors, types
 from io import StringIO
 from contextlib import redirect_stdout
-
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
@@ -538,49 +536,7 @@ async def ladder(client: Client, message: Message):
     await message.edit(ot)
 
 # Quotes
-def resize_image(input_img, output=None, img_type="PNG"):
-    if output is None:
-        output = BytesIO()
-        output.name = f"sticker.{img_type.lower()}"
-    with Image.open(input_img) as img:
-        # We used to use thumbnail(size) here, but it returns with a *max* dimension of 512,512
-        # rather than making one side exactly 512 so we have to calculate dimensions manually :(
-        if img.width == img.height:
-            size = (512, 512)
-        elif img.width < img.height:
-            size = (max(512 * img.width // img.height, 1), 512)
-        else:
-            size = (512, max(512 * img.height // img.width, 1))
-        img.resize(size).save(output, img_type)
-    output.seek(0)
-    return output
-
-def format_exc(e: Exception, hint: str = None):
-    traceback.print_exc()
-    if isinstance(e, errors.RPCError):
-        return (
-            f"<b>Telegram API error!</b>\n"
-            f"<code>[{e.CODE} {e.ID or e.NAME}] - {e.MESSAGE}</code>"
-        )
-    else:
-        if hint:
-            hint_text = f"\n\n<b>Hint: {hint}</b>"
-        else:
-            hint_text = ""
-        return (
-            f"<b>Error!</b>\n" f"<code>{e.__class__.__name__}: {e}</code>" + hint_text
-        )
-
-def with_reply(func):
-    async def wrapped(client: Client, message: types.Message):
-
-        if not message.reply_to_message:
-            await message.edit("<b>Reply to message is required</b>")
-        else:
-            return await func(client, message)
-    return wrapped
-
-@app.on_message(filters.command("q", prefixes=".") & filters.me)
+@Client.on_message(filters.command(["q", "quote"], prefix) & filters.me)
 @with_reply
 async def quote_cmd(client: Client, message: types.Message):
     if len(message.command) > 1 and message.command[1].isdigit():
@@ -591,13 +547,13 @@ async def quote_cmd(client: Client, message: types.Message):
             count = 15
     else:
         count = 1
- 
+
     is_png = "!png" in message.command or "!file" in message.command
     send_for_me = "!me" in message.command or "!ls" in message.command
     no_reply = "!noreply" in message.command or "!nr" in message.command
- 
+
     messages = []
- 
+
     async for msg in client.iter_history(
         message.chat.id, offset_id=message.reply_to_message.message_id, reverse=True
     ):
@@ -607,18 +563,18 @@ async def quote_cmd(client: Client, message: types.Message):
             break
         if no_reply:
             msg.reply_to_message = None
- 
+
         messages.append(msg)
- 
+
         if len(messages) >= count:
             break
- 
+
     if send_for_me:
         await message.delete()
-        message = await client.send_message("me", "<b>Генерация...</b>")
+        message = await client.send_message("me", "<b>Generating...</b>")
     else:
-        await message.edit("<b>Генерация...</b>")
- 
+        await message.edit("<b>Generating...</b>")
+
     url = "https://quotes.fl1yd.su/generate"
     params = {
         "messages": [
@@ -627,18 +583,18 @@ async def quote_cmd(client: Client, message: types.Message):
         "quote_color": "#162330",
         "text_color": "#fff",
     }
- 
+
     response = requests.post(url, json=params)
     if not response.ok:
         return await message.edit(
             f"<b>Quotes API error!</b>\n" f"<code>{response.text}</code>"
         )
- 
+
     resized = resize_image(
         BytesIO(response.content), img_type="PNG" if is_png else "WEBP"
     )
-    await message.edit("<b>Отправка...</b>")
- 
+    await message.edit("<b>Sending...</b>")
+
     try:
         func = client.send_document if is_png else client.send_sticker
         chat_id = "me" if send_for_me else message.chat.id
@@ -647,15 +603,15 @@ async def quote_cmd(client: Client, message: types.Message):
         await message.edit(format_exc(e))
     else:
         await message.delete()
- 
- 
-@app.on_message(filters.command("fq", prefixes=".") & filters.me)
+
+
+@app.on_message(filters.command("fq", ".") & filters.me)
 @with_reply
 async def fake_quote_cmd(client: Client, message: types.Message):
     is_png = "!png" in message.command or "!file" in message.command
     send_for_me = "!me" in message.command or "!ls" in message.command
     no_reply = "!noreply" in message.command or "!nr" in message.command
- 
+
     fake_quote_text = " ".join(
         [
             arg
@@ -663,10 +619,10 @@ async def fake_quote_cmd(client: Client, message: types.Message):
             if arg not in ["!png", "!file", "!me", "!ls", "!noreply", "!nr"]
         ]  # remove some special arg words
     )
- 
+
     if not fake_quote_text:
         return await message.edit("<b>Fake quote text is empty</b>")
- 
+
     q_message = await client.get_messages(
         message.chat.id, message.reply_to_message.message_id
     )
@@ -674,31 +630,31 @@ async def fake_quote_cmd(client: Client, message: types.Message):
     q_message.entities = None
     if no_reply:
         q_message.reply_to_message = None
- 
+
     if send_for_me:
         await message.delete()
         message = await client.send_message("me", "<b>Generating...</b>")
     else:
         await message.edit("<b>Generating...</b>")
- 
+
     url = "https://quotes.fl1yd.su/generate"
     params = {
         "messages": [await render_message(client, q_message)],
         "quote_color": "#162330",
         "text_color": "#fff",
     }
- 
+
     response = requests.post(url, json=params)
     if not response.ok:
         return await message.edit(
             f"<b>Quotes API error!</b>\n" f"<code>{response.text}</code>"
         )
- 
+
     resized = resize_image(
         BytesIO(response.content), img_type="PNG" if is_png else "WEBP"
     )
     await message.edit("<b>Sending...</b>")
- 
+
     try:
         func = client.send_document if is_png else client.send_sticker
         chat_id = "me" if send_for_me else message.chat.id
@@ -707,14 +663,16 @@ async def fake_quote_cmd(client: Client, message: types.Message):
         await message.edit(format_exc(e))
     else:
         await message.delete()
- 
- 
+
+
 files_cache = {}
+
+
 async def render_message(app: Client, message: types.Message) -> dict:
     async def get_file(file_id) -> str:
         if file_id in files_cache:
             return files_cache[file_id]
- 
+
         file_name = await app.download_media(file_id)
         with open(file_name, "rb") as f:
             content = f.read()
@@ -722,7 +680,7 @@ async def render_message(app: Client, message: types.Message) -> dict:
         data = base64.b64encode(content).decode()
         files_cache[file_id] = data
         return data
- 
+
     # text
     if message.photo:
         text = message.caption if message.caption else ""
@@ -732,7 +690,7 @@ async def render_message(app: Client, message: types.Message) -> dict:
         text = ""
     else:
         text = get_reply_text(message)
- 
+
     # media
     if message.photo:
         media = await get_file(message.photo.file_id)
@@ -740,7 +698,7 @@ async def render_message(app: Client, message: types.Message) -> dict:
         media = await get_file(message.sticker.file_id)
     else:
         media = ""
- 
+
     # entities
     entities = []
     if message.entities:
@@ -752,7 +710,7 @@ async def render_message(app: Client, message: types.Message) -> dict:
                     "type": entity.type,
                 }
             )
- 
+
     def move_forwards(msg: types.Message):
         if msg.forward_from:
             msg.from_user = msg.forward_from
@@ -765,14 +723,14 @@ async def render_message(app: Client, message: types.Message) -> dict:
             msg.from_user.id = 0
         if msg.forward_signature:
             msg.author_signature = msg.forward_signature
- 
+
     move_forwards(message)
- 
+
     # author
     author = {}
     if message.from_user and message.from_user.id != 0:
         from_user = message.from_user
- 
+
         author["id"] = from_user.id
         author["name"] = get_full_name(from_user)
         if message.author_signature:
@@ -792,7 +750,7 @@ async def render_message(app: Client, message: types.Message) -> dict:
                     if member.status == "administrator"
                     else ""
                 )
- 
+
         if from_user.photo:
             author["avatar"] = await get_file(from_user.photo.big_file_id)
         elif not from_user.photo and from_user.username:
@@ -824,28 +782,28 @@ async def render_message(app: Client, message: types.Message) -> dict:
         author["id"] = message.sender_chat.id
         author["name"] = message.sender_chat.title
         author["rank"] = "channel" if message.sender_chat.type == "channel" else ""
- 
+
         if message.sender_chat.photo:
             author["avatar"] = await get_file(message.sender_chat.photo.big_file_id)
         else:
             author["avatar"] = ""
     author["via_bot"] = message.via_bot.username if message.via_bot else ""
- 
+
     # reply
     reply = {}
     reply_msg = message.reply_to_message
     if reply_msg and not reply_msg.empty:
         move_forwards(reply_msg)
- 
+
         if reply_msg.from_user:
             reply["id"] = reply_msg.from_user.id
             reply["name"] = get_full_name(reply_msg.from_user)
         else:
             reply["id"] = reply_msg.sender_chat.id
             reply["name"] = reply_msg.sender_chat.title
- 
+
         reply["text"] = get_reply_text(reply_msg)
- 
+
     return {
         "text": text,
         "media": media,
@@ -853,8 +811,8 @@ async def render_message(app: Client, message: types.Message) -> dict:
         "author": author,
         "reply": reply,
     }
- 
- 
+
+
 def get_audio_text(audio: types.Audio) -> str:
     if audio.title and audio.performer:
         return f" ({audio.title} — {audio.performer})"
@@ -864,8 +822,8 @@ def get_audio_text(audio: types.Audio) -> str:
         return f" ({audio.performer})"
     else:
         return ""
- 
- 
+
+
 def get_reply_text(reply: types.Message) -> str:
     return (
         "📷 Photo" + ("\n" + reply.caption if reply.caption else "")
@@ -929,23 +887,23 @@ def get_reply_text(reply: types.Message) -> str:
         if reply.channel_chat_created
         else reply.text or "unsupported message"
     )
- 
- 
+
+
 def get_poll_text(poll: types.Poll) -> str:
     text = get_reply_poll_text(poll) + "\n"
- 
+
     text += poll.question + "\n"
     for option in poll.options:
         text += f"- {option.text}"
         if option.voter_count > 0:
             text += f" ({option.voter_count} voted)"
         text += "\n"
- 
+
     text += f"Total: {poll.total_voter_count} voted"
- 
+
     return text
- 
- 
+
+
 def get_reply_poll_text(poll: types.Poll) -> str:
     if poll.is_anonymous:
         text = "📊 Anonymous poll" if poll.type == "regular" else "📊 Anonymous quiz"
@@ -953,10 +911,10 @@ def get_reply_poll_text(poll: types.Poll) -> str:
         text = "📊 Poll" if poll.type == "regular" else "📊 Quiz"
     if poll.is_closed:
         text += " (closed)"
- 
+
     return text
- 
- 
+
+
 def get_full_name(user: types.User) -> str:
     name = user.first_name
     if user.last_name:
