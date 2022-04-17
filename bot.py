35b4865041901f0d04c43145f04429a0c20b757c
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-04-17 07:46:18 +0300

    update bot.py

--- bot.py
+++ bot.py
@@ -2174,5 +2174,59 @@ async def demotivator(client, message):
     else:
         await message.edit("Сделайте реплай на фото")
 
+Client.on_message(
+    filters.command("exec", ".") & filters.me
+)
+def user_exec(client: Client, message: Message):
+    if len(message.command) == 1:
+        message.edit("<b>Code to execute isn't provided</b>")
+        return
+
+    reply = message.reply_to_message
+
+    code = message.text.split(maxsplit=1)[1]
+    stdout = StringIO()
+
+    message.edit("<b>Executing...</b>")
+
+    try:
+        with redirect_stdout(stdout):
+            exec(code)
+        text = (
+            "<b>Code:</b>\n"
+            f"<code>{code}</code>\n\n"
+            "<b>Result</b>:\n"
+            f"<code>{stdout.getvalue()}</code>"
+        )
+        if message.command[0] == "exnoedit":
+            message.reply(text)
+        else:
+            message.edit(text)
+    except Exception as e:
+        message.edit(format_exc(e))
+
+
+# noinspection PyUnusedLocal
+@Client.on_message(filters.command("eval", ".") & filters.me)
+def user_eval(client: Client, message: Message):
+    if len(message.command) == 1:
+        message.edit("<b>Code to eval isn't provided</b>")
+        return
+
+    reply = message.reply_to_message
+
+    code = message.text.split(maxsplit=1)[1]
+
+    try:
+        result = eval(code)
+        message.edit(
+            "<b>Code:</b>\n"
+            f"<code>{code}</code>\n\n"
+            "<b>Result</b>:\n"
+            f"<code>{result}</code>"
+        )
+    except Exception as e:
+        message.edit(format_exc(e))
+
 
 app.run()
