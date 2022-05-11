bda459accfae62bc17307fdc68447f3707ffc09b
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-11 15:40:57 +0300

    update whois.py

--- plugins/whois.py
+++ plugins/whois.py
@@ -1,11 +1,23 @@
+from pyrogram import Client, filters
+from plugins.settings.main_settings import module_list, file_list
+
+from prefix import my_prefix
+prefix = my_prefix()
+from pyrogram.raw import functions, types
+from pyrogram.types import Message, ChatPermissions
+from pyrogram.utils import (
+    get_channel_id,
+    MAX_USER_ID,
+    MIN_CHAT_ID,
+    MAX_CHANNEL_ID,
+    MIN_CHANNEL_ID,
+)
+
 @Client.on_message(filters.command("infofull", prefixes=prefix) & filters.me)
-
 async def info(client: Client, message: Message):
-
     timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
     log = logi + timnow + "\n╰ Полная информация"
     await Client.send_message("sawUSERBOT_LOGGERbot", log)
-
     if message.reply_to_message:
         username = message.reply_to_message.from_user.username
         id = message.reply_to_message.from_user.id
