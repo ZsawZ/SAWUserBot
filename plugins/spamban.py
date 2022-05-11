981b13da6248a01a1f47d8482d1a696d9519c084
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-11 15:36:02 +0300

    update spamban.py

--- plugins/spamban.py
+++ plugins/spamban.py
@@ -1,8 +1,20 @@
+from pyrogram import Client, filters
+from plugins.settings.main_settings import module_list, file_list
+
+from prefix import my_prefix
+prefix = my_prefix()
+from pyrogram.raw import functions, types
+from pyrogram.types import Message, ChatPermissions
+from pyrogram.utils import (
+    get_channel_id,
+    MAX_USER_ID,
+    MIN_CHAT_ID,
+    MAX_CHANNEL_ID,
+    MIN_CHANNEL_ID,
+)
+
 @Client.on_message(filters.command("spamban", prefixes=prefix) & filters.me)
-
 async def spamban(client: Client, message: Message):
-
-
     timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
     log = logi + timnow + "\n╰ Проверка нарушений"
     await Client.send_message("sawUSERBOT_LOGGERbot", log)
