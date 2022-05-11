23cb30749f71bacb18bb2577dedea91e7d91e587
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-11 15:33:39 +0300

    update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -1,3 +1,18 @@
+from pyrogram import Client, filters
+from plugins.settings.main_settings import module_list, file_list
+
+from prefix import my_prefix
+prefix = my_prefix()
+from pyrogram.raw import functions, types
+from pyrogram.types import Message, ChatPermissions
+from pyrogram.utils import (
+    get_channel_id,
+    MAX_USER_ID,
+    MIN_CHAT_ID,
+    MAX_CHANNEL_ID,
+    MIN_CHANNEL_ID,
+)
+
 @Client.on_message(filters.command("restart", prefixes=prefix) & filters.me)
 async def restartt(client: Client, message: Message):
     now = datetime.datetime.now()
