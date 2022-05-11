26bd06e5f8094b3fc307e9e46da5c9ebf1c6e811
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-11 15:38:28 +0300

    update time.py

--- plugins/time.py
+++ plugins/time.py
@@ -1,7 +1,20 @@
+from pyrogram import Client, filters
+from plugins.settings.main_settings import module_list, file_list
+
+from prefix import my_prefix
+prefix = my_prefix()
+from pyrogram.raw import functions, types
+from pyrogram.types import Message, ChatPermissions
+from pyrogram.utils import (
+    get_channel_id,
+    MAX_USER_ID,
+    MIN_CHAT_ID,
+    MAX_CHANNEL_ID,
+    MIN_CHANNEL_ID,
+)
+
 @Client.on_message(filters.command("time", prefixes=prefix) & filters.me)
-
 async def time(client: Client, message: Message):
-
     now = datetime.datetime.now()
     timnow = now.strftime("%d.%m.%Y\nВремя %H:%M:%S")
     timenow = "Текущая дата : " + timnow
