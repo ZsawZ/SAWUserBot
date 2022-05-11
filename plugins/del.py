d7bb2db441b71314ae775fd735d3252ab9c6e378
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-11 15:15:48 +0300

    update del.py

--- plugins/del.py
+++ plugins/del.py
@@ -1,3 +1,9 @@
+from pyrogram import Client, filters
+from plugins.settings.main_settings import module_list, file_list
+
+from prefix import my_prefix
+prefix = my_prefix()
+
 @Client.on_message(filters.command("del", prefixes=prefix) & filters.me)
 async def delete_messages(client: Client, message: Message):
 
