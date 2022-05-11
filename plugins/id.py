1a76a61048dc7e0b383bdfe81ab2ebfed1a5639d
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-11 15:24:49 +0300

    update id.py

--- plugins/id.py
+++ plugins/id.py
@@ -1,7 +1,20 @@
+from pyrogram import Client, filters
+from plugins.settings.main_settings import module_list, file_list
+
+from prefix import my_prefix
+prefix = my_prefix()
+from pyrogram.raw import functions, types
+from pyrogram.types import Message, ChatPermissions
+from pyrogram.utils import (
+    get_channel_id,
+    MAX_USER_ID,
+    MIN_CHAT_ID,
+    MAX_CHANNEL_ID,
+    MIN_CHANNEL_ID,
+)
+
 @Client.on_message(filters.command("id", prefixes=prefix) & filters.me)
-
 async def id(client: Client, message: Message):
-
     now = datetime.datetime.now()
     timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
     log = logi + timnow + "\n╰ Комманда id"
