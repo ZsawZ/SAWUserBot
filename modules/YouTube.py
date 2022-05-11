7f3adb895c017ed6922dacb4d43b8e274de2ddd1
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-11 12:44:32 +0300

    update YouTube.py

--- modules/YouTube.py
+++ modules/YouTube.py
@@ -1,13 +1,11 @@
+Client = Client("my_account", api_id=api_id, api_hash=api_hash, device_model=device_mod, plugins=plugins).run()
+
 @Client.on_message(filters.command("yt", prefixes=prefix) & filters.me)
-
 async def yt(client, message):
-
     linked = message.command[1]
-
     timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
     log = logi + timnow + "\n╰ Запрос на скачивания видео"
     await Client.send_message("sawUSERBOT_LOGGERbot", log)
-
     await message.edit("Скачивание видео...")
     ydl_opts = { "outtmpl": "video.mp4", }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -19,11 +17,9 @@ async def yt(client, message):
 
 @Client.on_message(filters.command("myt", prefixes=prefix) & filters.me)
 async def myt(client, message):
-
     timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
     log = logi + timnow + "\n╰ Запрос на скачивание звука"
     await Client.send_message("sawUSERBOT_LOGGERbot", log)
-
     myth = "youtube-dl -f 140 " + message.command[1] + " -o music.m4a"
     await message.edit("Скачивание аудиодорожки...")
     os.system(myth)
