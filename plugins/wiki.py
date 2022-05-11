1b01c853a27ad394855b5415188c8c73173ca0ca
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-11 15:41:25 +0300

    update wiki.py

--- plugins/wiki.py
+++ plugins/wiki.py
@@ -1,11 +1,23 @@
+from pyrogram import Client, filters
+from plugins.settings.main_settings import module_list, file_list
+
+from prefix import my_prefix
+prefix = my_prefix()
+from pyrogram.raw import functions, types
+from pyrogram.types import Message, ChatPermissions
+from pyrogram.utils import (
+    get_channel_id,
+    MAX_USER_ID,
+    MIN_CHAT_ID,
+    MAX_CHANNEL_ID,
+    MIN_CHANNEL_ID,
+)
+
 @Client.on_message(filters.command("wiki", prefixes=prefix) & filters.me)
-
 async def wiki(client: Client, message: Message):
-
     timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
     log = logi + timnow + "\n╰ Поиск в википедии"
     await Client.send_message("sawUSERBOT_LOGGERbot", log)
-
     lang = message.command[1]
     user_request = " ".join(message.command[2:])
     await message.edit("<b>Ищем инфу</b>")
