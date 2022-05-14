49ebe858f775c489785ba8b630e62607f5c8bfc7
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-14 18:50:38 +0300

    update admin_commands.py

--- plugins/admin_commands.py
+++ plugins/admin_commands.py
@@ -12,6 +12,8 @@ from pyrogram.utils import (
     MAX_CHANNEL_ID,
     MIN_CHANNEL_ID,
 )
+import time, random, datetime, asyncio, sys, wikipedia, requests, json, colorama, requests, youtube_dl, subprocess, configparser
+
 
 def get_arg(message):
 
