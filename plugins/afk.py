9459a0d3d7240d4b2be9c819e0c19fdcbfc02ea9
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-14 18:50:49 +0300

    update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -12,6 +12,8 @@ from pyrogram.utils import (
     MAX_CHANNEL_ID,
     MIN_CHANNEL_ID,
 )
+import time, random, datetime, asyncio, sys, wikipedia, requests, json, colorama, requests, youtube_dl, subprocess, configparser
+
 
 @Client.on_message (filters.command("afk", prefixes=prefix) & filters.me)
 async def afk(client: Client, message: Message):
