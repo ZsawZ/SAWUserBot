0da55fd0b8fbe0c0a6512ad52c7c0ab37c3948aa
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-15 16:11:29 +0300

    update spamban.py

--- plugins/spamban.py
+++ plugins/spamban.py
@@ -16,12 +16,11 @@ from pyrogram.utils import (
     MIN_CHANNEL_ID,
 )
 
+now = datetime.datetime.now()
+timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+
 @Client.on_message(filters.command("spamban", prefixes=prefix) & filters.me)
 async def spamban(client: Client, message: Message):
-    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
-    log = logi + timnow + "\n╰ Проверка нарушений"
-    await Client.send_message("sawUSERBOT_LOGGERbot", log)
-
     await message.edit("Чекаю твой акк на наличие спамбана")
     await Client.send_message("spambot", "/start")
     await asyncio.sleep(1)
