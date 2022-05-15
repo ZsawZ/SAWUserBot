b002eed2841bda4107634bef82f64a727eaee0b3
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-15 17:58:04 +0300

    update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -1,36 +1,69 @@
+import os
+import zipfile
+import wget
 from pyrogram import Client, filters
+from pyrogram.types import Message
 from plugins.settings.main_settings import module_list, file_list
-import time, random, datetime, asyncio, sys, wikipedia, requests, json, colorama, requests, youtube_dl, subprocess, configparser
-from time import sleep, perf_counter, time
 
-from time import time
 from prefix import my_prefix
 prefix = my_prefix()
-from pyrogram.raw import functions, types
-from pyrogram.types import Message, ChatPermissions
-from pyrogram.utils import (
-    get_channel_id,
-    MAX_USER_ID,
-    MIN_CHAT_ID,
-    MAX_CHANNEL_ID,
-    MIN_CHANNEL_ID,
-)
-now = datetime.datetime.now()
-timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
 
+
+async def restart(message: Message, restart_type):
+    if restart_type == "update":
+        text = "1"
+    else:
+        text = "2"
+    try:
+        await os.execvp(
+            "python3",
+            [
+                "python3",
+                "./main.py",
+                f"{message.chat.id}",
+                f"{message.message_id}",
+                f"{text}",
+            ],
+        )
+    except:
+        await os.execvp(
+            "python",
+            [
+                "python",
+                "./main.py",
+                f"{message.chat.id}",
+                f"{message.message_id}",
+                f"{text}",
+            ],
+        )
+
+
+# Restart
 @Client.on_message(filters.command("restart", prefixes=prefix) & filters.me)
-async def restartt(client: Client, message: Message):
+async def restart_get(client, message):
+    try:
+        await message.edit("**Restarting userbot...**")
+        await restart(message, restart_type="restart")
+    except:
+        await message.edit("**An error occured...**")
 
-    await message.delete()
-    await Client.send_audio(message.chat.id, "stop.ogg", "<code>Перезагрузка...</code>")
-    await restart(message, restart_type="restart")
 
-@Client.on_message(filters.command("update", prefixes=prefix) & filters.me)
-async def updatte(client: Client, message: Message):
-    await message.edit("<code>Обновление...</code>")
-    subprocess.call(["bash", "update.sh"])
-    await message.edit("<code>Юзербот успешно обновлён!</code>")
-    await restart(message, restart_type="1")
+# Update
+@Client.on_message(filters.command('update', prefixes=prefix) & filters.me)
+async def update(client, message):
+    try:
+        await message.edit('**Updating...**')
+        link = "https://github.com/ZsawZ/SAWUserBot/archive/refs/heads/main.zip"
+        wget.download(link, 'temp/archive.zip')
+        with zipfile.ZipFile("temp/archive.zip", "r") as zip_ref:
+            zip_ref.extractall("./..")
+        os.remove("temp/archive.zip")
 
-module_list['Restarter'] = f'{prefix}restart | {prefix}update'
-file_list['Restarter'] = 'restart.py'
+        await message.edit('**Userbot succesfully updated\nRestarting...**')
+        await restart(message, restart_type="update")
+    except:
+        await message.edit("**An error occured...**")
+
+
+module_list['Restarter'] = f'{prefix}update | {prefix}restart'
+file_list['Restarter'] = 'restarter.py'
