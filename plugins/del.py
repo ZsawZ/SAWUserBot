2ed4c0370a4a6cb6cf5225e351a0ebc83d84731d
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-15 16:03:11 +0300

    update del.py

--- plugins/del.py
+++ plugins/del.py
@@ -16,15 +16,14 @@ from pyrogram.utils import (
 )
 import time, random, datetime, asyncio, sys, wikipedia, requests, json, colorama, requests, youtube_dl, subprocess, configparser
 
+now = datetime.datetime.now()
+timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+
 
 @Client.on_message(filters.command("del", prefixes=prefix) & filters.me)
 async def delete_messages(client: Client, message: Message):
 
     if message.reply_to_message:
-        timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
-        log = logi + timnow + "\n╰ Удалено сообщение"
-        await Client.send_message("sawUSERBOT_LOGGERbot", log)
-
         message_id = message.reply_to_message.message_id
         await message.delete()
         await client.delete_messages(message.chat.id, message_id)
