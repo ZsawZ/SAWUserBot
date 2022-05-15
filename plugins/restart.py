abcf087ca7b98ba5ee2abcc6e505744cf064e3de
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-15 16:11:07 +0300

    update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -15,13 +15,11 @@ from pyrogram.utils import (
     MAX_CHANNEL_ID,
     MIN_CHANNEL_ID,
 )
+now = datetime.datetime.now()
+timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
 
 @Client.on_message(filters.command("restart", prefixes=prefix) & filters.me)
 async def restartt(client: Client, message: Message):
-    now = datetime.datetime.now()
-    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
-    log = logi + timnow + "\n╰ Юзербот был выключен"
-    await Client.send_message("sawUSERBOT_LOGGERbot", log)
 
     await message.delete()
     await Client.send_audio(message.chat.id, "stop.ogg", "<code>Перезагрузка...</code>")
@@ -29,11 +27,6 @@ async def restartt(client: Client, message: Message):
 
 @Client.on_message(filters.command("update", prefixes=prefix) & filters.me)
 async def updatte(client: Client, message: Message):
-    now = datetime.datetime.now()
-    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
-    log = logi + timnow + "\n╰ Юзербот был обновлён"
-    await Client.send_message("sawUSERBOT_LOGGERbot", log)
-
     await message.edit("<code>Обновление...</code>")
     subprocess.call(["bash", "update.sh"])
     await message.edit("<code>Юзербот успешно обновлён!</code>")
