3ff0ad25e41f779d7f8de8811b0cb8338a60ece9
ZSAW <66517823+ZsawZ@users.noreply.github.com>
2022-05-16 11:34:45 +0300

    update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -4,7 +4,7 @@ import wget
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from plugins.settings.main_settings import module_list, file_list
-
+import subprocess
 from prefix import my_prefix
 prefix = my_prefix()
 
@@ -52,14 +52,9 @@ async def restart_get(client, message):
 @Client.on_message(filters.command('update', prefixes=prefix) & filters.me)
 async def update(client, message):
     try:
-        await message.edit('**Updating...**')
-        link = "https://github.com/ZsawZ/SAWUserBot/archive/refs/heads/main.zip"
-        wget.download(link, 'temp/archive.zip')
-        with zipfile.ZipFile("temp/archive.zip", "r") as zip_ref:
-            zip_ref.extractall("./..")
-        os.remove("temp/archive.zip")
-
-        await message.edit('**Userbot succesfully updated\nRestarting...**')
+        await message.edit("<code>Обновление...</code>")
+        subprocess.call(["bash", "update.sh"])
+        await message.edit("<code>Юзербот успешно обновлён!</code>")
         await restart(message, restart_type="update")
     except:
         await message.edit("**An error occured...**")
